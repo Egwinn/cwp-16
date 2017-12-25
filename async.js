2d66e7d37df5180bb5b4939ccd01eb65254a92b0
Marina <dubovik.m14@gmail.com>
2017-12-25 21:01:56 +0200

    add task01-3

--- async.js
+++ async.js
@@ -21,4 +21,12 @@ async function WorkWork() {
     ]);
     console.log('------- task01-2 -------')
     console.log(country.data.results[0].formatted_address.split(", ")[1]);
+
+    // task01-3
+    let street = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=Via Nicola Salvi`);
+    console.log('------- task01-3 -------')
+    console.log(street.data.results[0].formatted_address);
+    street.data.results[0].address_components.forEach((component) => {
+        console.log(`   -${component.long_name}`);
+    });
 }
