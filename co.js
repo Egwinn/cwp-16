ec604d85e1000933107bce66ed76b4ebeb7aeb37
Marina <dubovik.m14@gmail.com>
2017-12-25 21:31:19 +0200

    add task02-2

--- co.js
+++ co.js
@@ -15,10 +15,29 @@ async function WorkWork() {
         ]);
     }).then(function (value) {
         console.log(geolib.getDistance(value[0].data.results[0].geometry.location,
-                           value[1].data.results[0].geometry.location));
+            value[1].data.results[0].geometry.location));
     }, function (err) {
         console.error(err.stack);
     });
 
-
+    // task02-2
+    console.log('------- task02-2 -------');
+    await co(function* () {
+        return yield Promise.mapSeries([
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Minsk`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Copenhagen`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Oslo`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Brussel`
+        ], async function (item) {
+            return await axios.get(item);
+        });
+    }).then(function (value) {
+        let cities = {};
+        value.map((city) => {
+            cities[city.data.results[0].formatted_address] = city.data.results[0].geometry.location;
+        });
+        console.log(geolib.findNearest(cities['Minsk, Belarus'], cities, 1));
+    }, function (err) {
+        console.error(err.stack);
+    });
 }
